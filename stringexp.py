441047d055a93c99cf11718672c7f66603ade672
NITISH GUPTA <sanjanagupta894877@gmail.com>
2019-02-10 19:25:14 +0530

    update by string

---
+++ stringexp.py
@@ -0,0 +1,38 @@
+str="welcome in python"
+print(str)
+print(str.capitalize())
+print(str.count("o"))
+print(str.count("python"))
+print(str.endswith("thon"))
+str1="techsrijan consultany service"
+print(str1.find("srijan"))
+print(str1.find("s"))
+print(len(str1))
+print(str1.split("s"))
+print(str1.title())
+print(str1.lower())
+print(str1.upper())
+print(str1.islower())
+str2="hello"
+print(str2.isalpha())
+print(str1.split())
+print(str2.islower())
+print(str1.isupper())
+print(str1.swapcase())
+str3="her name is sanjana and sanjana is good girl"
+print(str3)
+print(str3.replace("sanjana","sanju"))
+print(str3.replace("i","z"))
+str4="8948773645a"
+print(str4.isdigit())
+str5="8946773645"
+print(str5.isdigit())
+print(str3.isalpha())
+print(str2.isalpha())
+str6="    !!!hello!!!"
+print(str6)
+print(str6.strip())
+str7="       !!!hello!!!"
+print(str7.strip("!"))
+print(str7.lstrip())
+print(str7.rsplit())
